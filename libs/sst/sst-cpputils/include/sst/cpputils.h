6b1002dff591e1e1d657dde59e859f5f12ab6ec4
colton8dio <colton@8dio.com>
2022-12-30 14:51:16 -0500

    initial +

---
+++ libs/sst/sst-cpputils/include/sst/cpputils.h
@@ -0,0 +1,11 @@
+/*
+ * sst-cpputils
+ * Released under the MIT License
+ *
+ * This header includes the entire suite of CPP utils. You can include individual ones
+ * by choosing the files below if you prefer
+ */
+
+#include "sst/cpputils/algorithms.h"
+#include "sst/cpputils/iterators.h"
+#include "sst/cpputils/ring_buffer.h"
